6b53676f8e3c157c5deb613f487852d34a5f04d5
sindbis <sivabalan.manivannan@gmail.com>
2015-04-20 08:14:46 -0500

    Refactored

--- wiki.go
+++ wiki.go
@@ -9,9 +9,44 @@ import (
 	"strings"
 )
 
+//Takes in url string and returns JSON of wiki summary
+func getSummary(url string) map[string]interface{} {
+
+	r, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	//Get the body of the request
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//Loads JSON into the gabs Parser
+	jsonParsed, err := gabs.ParseJSON(body)
+	if err != nil {
+		panic(err)
+	}
+
+	children, err := jsonParsed.Path("query.pages").Children()
+	if err != nil {
+		panic(err)
+	}
+	// This child contains relevant information (title, summary)
+	child := children[0].Data()
+
+	//Cast into a map of interfaces
+	data := child.(map[string]interface{})
+
+	return data
+}
+
 func main() {
 
 	var query string
+
 	//Checks for right number of command line arguments
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: wiki <query> ")
@@ -28,18 +63,8 @@ func main() {
 	url := "https://en.wikipedia.org/w/api.php?continue=&action=query&titles=placeholder&prop=extracts&exintro=&explaintext=&format=json&redirects"
 
 	url = strings.Replace(url, "placeholder", query, -1)
-	r, _ := http.Get(url)
-	defer r.Body.Close()
-	body, _ := ioutil.ReadAll(r.Body)
 
-	//Loads JSON into the gabs Parser
-	jsonParsed, err := gabs.ParseJSON(body)
-	if err != nil {
-		panic(err)
-	}
-	children, _ := jsonParsed.Path("query.pages").Children()
-	child := children[0].Data()            // This child contains relevant information (title, summary)
-	data := child.(map[string]interface{}) //Cast into a map of interfaces
+	data := getSummary(url)
 
 	//Output to terminal
 	fmt.Println("\n", data["title"], "\n\n", data["extract"], "\n")
